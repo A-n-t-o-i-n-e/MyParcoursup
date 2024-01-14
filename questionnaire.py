08dc9d4001b799875eb6971595f0a0d774f5256b
Antoine Chabenat <antoine.chabenat.code@gmail.com>
2024-01-14 15:22:24 +0100

    traitement voeux json
    je me suis rendu compte que pour ne pas avoir à créer 1000 json différents pour chaque paramètre il fallait créer un fichier centraliser. ce qui va alléger le code de traitments_voeux()

--- questionnaire.py
+++ questionnaire.py
@@ -45,12 +45,17 @@ def traitement_voeux(filieres, resultat_questionaire_voeux, domaines):
                     filieres_notees[filiere] += 1-i/len(resultat_questionaire_voeux[0])
 
     # duree etude
+    
+    # alternance 
     if resultat_questionaire_voeux[1]:
         pass
-    # alternance 
     # autonome
+    if resultat_questionaire_voeux[1]:
+        pass
     # dans un lycee 
-
+    if resultat_questionaire_voeux[1]:
+        pass
+    
     return filieres_notees
             
 
