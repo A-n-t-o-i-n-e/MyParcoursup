61ddf263c91ebfff2fa743859cd97a65831bc294
Chabenat <antoine.chabenat.code@gmail.com>
2024-01-18 16:00:02 +0100

    Update questionnaire.py

--- questionnaire.py
+++ questionnaire.py
@@ -37,9 +37,15 @@ def traitement_voeux(resultat_questionaire_voeux, traitement_voeux):
 
     key = ["fili", "domaine", "duree_etude", "alternance", "lycee"]
     
-    for filiere in traitement_voeux:
-        #complete 
-        pass
+    # parcours des filiere 
+    for i_filiere, filiere in enumerate(traitement_voeux):
+        #parcours des domaines des resultats
+        for i_domaine, domaine in enumerate(resultat_questionaire_voeux[0]):
+            if filiere['domaine'] == domaine:  # le res du user est dans la filiere parcouru
+                filieres_notees[filiere['fili']] = 1 - i_domaine / len(resultat_questionaire_voeux[0])
+        # parcours des duree_etude des resultats
+        for i_duree_etude, duree_etude in enumerate(resultat_questionaire_voeux[1 ]):
+
     return filieres_notees
             
 
