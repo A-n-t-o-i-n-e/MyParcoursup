bb3c4fea07421e65e67636ae76ceb36585f3e6a6
Antoine Chabenat <antoine.chabenat.code@gmail.com>
2024-01-21 13:30:04 +0100

    Update formation.py

--- test/formation.py
+++ test/formation.py
@@ -15,19 +15,25 @@ def get_all_formation(data, parametre = 'form_lib_voe_acc'):
 def get_but(data):
     for voeux in data:
         if voeux['fil_lib_voe_acc']  == 'Informatique' and voeux['fili']  == 'BUT'and voeux['dep'] == '33':
-            print(voeux)
+            print(dumps(voeux, indent=2))
+
+def get_fil_lib_voe_acc_of_form_lib_voe_acc(data, parametre = 'BUT - Production'):
+    list_ = []
+    for veaux in data:
+        if (veaux['fil_lib_voe_acc'] not in list_) and veaux['form_lib_voe_acc'] == parametre:
+            list_.append(veaux['fil_lib_voe_acc'])
+    list_.sort()
+    return list_
 
 
 # mis en forme des données
 print(
     dumps(
-        get_all_formation(data, 'fil_lib_voe_acc'), 
+        get_fil_lib_voe_acc_of_form_lib_voe_acc(data, "Classe préparatoire scientifique"), 
         indent=2
     ),
-    len(get_all_formation(data, 'fil_lib_voe_acc'))
+    len(get_fil_lib_voe_acc_of_form_lib_voe_acc(data, "Classe préparatoire scientifique"))
 )
 
 """with open('formation.json',  'w') as fp:
     dump(get_all_formation(data), fp, sort_keys=True, indent=2)"""
-
-print(get_but(data))
